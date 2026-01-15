1ceab4f853fecfb46b5ef5a2765fe9767e169f22
ThatianaMattos <thatianamattos25@gmail.com>
2026-01-15 20:33:44 -0300

    feat: rotas, login e cadastro com tailwind

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,53 +1,51 @@
 import {
-  FacebookLogoIcon,
+  GithubLogoIcon,
   InstagramLogoIcon,
   LinkedinLogoIcon,
 } from "@phosphor-icons/react";
 
 function Footer() {
-  let data = new Date().getFullYear();
+  const data = new Date().getFullYear();
 
   return (
-    <>
-      <div className="flex justify-center bg-indigo-900 text-white">
-        <div className="container flex flex-col items-center py-6 text-center gap-2">
-          <p className="text-xl font-bold">
-            Blog Pessoal Generation | Copyright: {data}
-          </p>
+    <div className="flex justify-center bg-indigo-900 text-white">
+      <div className="container flex flex-col items-center py-6 text-center gap-2">
+        <p className="text-xl font-bold">
+          Blog Pessoal Generation | Copyright: {data}
+        </p>
 
-          <p className="text-lg">Acesse nossas redes sociais</p>
+        <p className="text-lg">Acesse minhas redes sociais</p>
 
-          <div className="flex gap-4 mt-2">
-            <a
-              href="https://www.linkedin.com"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-indigo-200 transition-all duration-300 hover:scale-110"
-            >
-              <LinkedinLogoIcon size={48} weight="bold" />
-            </a>
+        <div className="flex gap-4 mt-2">
+          <a
+            href="https://www.linkedin.com/in/thatiana-mattos/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-indigo-200 transition-all duration-300 hover:scale-110"
+          >
+            <LinkedinLogoIcon size={48} weight="bold" />
+          </a>
 
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-indigo-200 transition-all duration-300 hover:scale-110"
-            >
-              <InstagramLogoIcon size={48} weight="bold" />
-            </a>
+          <a
+            href="https://www.instagram.com/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-indigo-200 transition-all duration-300 hover:scale-110"
+          >
+            <InstagramLogoIcon size={48} weight="bold" />
+          </a>
 
-            <a
-              href="https://www.facebook.com"
-              target="_blank"
-              rel="noreferrer"
-              className="hover:text-indigo-200 transition-all duration-300 hover:scale-110"
-            >
-              <FacebookLogoIcon size={48} weight="bold" />
-            </a>
-          </div>
+          <a
+            href="https://github.com/ThatianaMattos"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-indigo-200 transition-all duration-300 hover:scale-110"
+          >
+            <GithubLogoIcon size={48} weight="bold" />
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,49 +1,51 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
-    <>
-      <div className="w-full flex justify-center py-4 bg-indigo-900 text-white sticky top-0 z-50 shadow-lg">
-        <div className="container flex justify-between text-lg mx-8">
-          <h1 className="font-bold text-xl">Blog Pessoal</h1>
+    <div className="w-full flex justify-center py-4 bg-indigo-900 text-white sticky top-0 z-50 shadow-lg">
+      <div className="container flex justify-between text-lg mx-8">
+        <Link to="/home" className="font-bold text-xl">
+          Blog Pessoal
+        </Link>
 
-          <div className="flex gap-4">
-            <a
-              href="#"
-              className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
-            >
-              Postagens
-            </a>
+        <div className="flex gap-4">
+          <Link
+            to="/postagens"
+            className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
+          >
+            Postagens
+          </Link>
 
-            <a
-              href="#"
-              className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
-            >
-              Temas
-            </a>
+          <Link
+            to="/temas"
+            className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
+          >
+            Temas
+          </Link>
 
-            <a
-              href="#"
-              className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
-            >
-              Cadastrar tema
-            </a>
+          <Link
+            to="/cadastrartema"
+            className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
+          >
+            Cadastrar tema
+          </Link>
 
-            <a
-              href="#"
-              className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
-            >
-              Perfil
-            </a>
+          <Link
+            to="/perfil"
+            className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
+          >
+            Perfil
+          </Link>
 
-            <a
-              href="#"
-              className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
-            >
-              Sair
-            </a>
-          </div>
+          <Link
+            to="/"
+            className="px-3 py-1 rounded hover:bg-indigo-800 hover:underline transition-all duration-300"
+          >
+            Sair
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
