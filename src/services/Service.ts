0c706b00b8db9c9023f82273120cb13ffde94a8d
ThatianaMattos <thatianamattos25@gmail.com>
2026-01-19 18:46:45 -0300

    feat: integra listagem de temas com backend

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -7,7 +7,7 @@ const api = axios.create({
 export const cadastrarUsuario = async (
   url: string,
   dados: Object,
-  setDados: Function
+  setDados: Function,
 ) => {
   const resposta = await api.post(url, dados);
   setDados(resposta.data);
@@ -15,5 +15,14 @@ export const cadastrarUsuario = async (
 
 export const login = async (url: string, dados: Object, setDados: Function) => {
   const resposta = await api.post(url, dados);
+  setDados(resposta.data);
+};
+
+export const buscar = async (
+  url: string,
+  setDados: Function,
+  header: Object,
+) => {
+  const resposta = await api.get(url, header);
   setDados(resposta.data);
 };
